7b7fe49b9c39078760bac8d25143861315e1e24d
shivam <shivam.hms@gmail.com>
2023-02-03 12:52:18 +0530

    just trying

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import './App.css';
+
+function App() {
+  
+  return (
+    <>
+    <div className="App">
+    <marquee> <h1> Hello Shivam Kumar</h1></marquee>
+    
+    </div>
+    </>
+  );
+}
+
+export default App;
